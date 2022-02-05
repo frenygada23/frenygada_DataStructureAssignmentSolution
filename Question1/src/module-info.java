aad49bf7ecd3fe04d6496101a713e0182e7d1bd8
frenygada23 <94836026+frenygada23@users.noreply.github.com>
2022-02-05 19:15:54 +0530

    Add files via upload

---
+++ Question1/src/module-info.java
@@ -0,0 +1,2 @@
+module GradedAssignment_3 {
+}
---
+++ Question2/src/driver/Node.java
@@ -0,0 +1,64 @@
+package driver;
+
+class Node {
+        int val;
+	    Node left, right;
+	    
+	    Node(int item){
+	        val = item;
+	        left = right = null;
+	    }
+	}
+	class Main{
+	    public static Node node;
+	    static Node prevNode = null;
+	    static Node headNode = null;
+	   
+	    static void convertToSkewed(Node root){
+	       
+	        
+	        if(root == null){
+	            return;
+	        }
+	       
+	        convertToSkewed(root.left);
+	        
+	        Node rightNode = root.right;
+	       
+	        if(headNode == null){
+	            headNode = root;
+	            root.left = null;
+	            prevNode = root;
+	        }
+	        else{
+	            prevNode.right = root;
+	            root.left = null;
+	            prevNode = root;
+	        }
+	       
+	        convertToSkewed(rightNode);
+	        
+	    }
+	   
+	    static void traverseRightSkewed(Node root){
+	        if(root == null){
+	            return;
+	        }
+	        System.out.print(root.val + " ");
+	        traverseRightSkewed(root.right);       
+	    }
+	   
+	   
+	    public static void main (String[] args){
+	        Main tree = new Main();
+	        tree.node = new Node(50);
+	        tree.node.left = new Node(30);
+	        tree.node.right = new Node(60);
+	        tree.node.left.left = new Node(10);
+	        tree.node.left.right= new Node(40);
+	     
+	        convertToSkewed(node);
+	        traverseRightSkewed(headNode);
+	    }
+	}
+
