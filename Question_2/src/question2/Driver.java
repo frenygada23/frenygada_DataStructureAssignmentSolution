e820501c47b3e041988996e94d88f83ad07459c3
frenygada23 <94836026+frenygada23@users.noreply.github.com>
2022-02-06 23:05:16 +0530

    Add files via upload

---
+++ Question_2/src/question2/Driver.java
@@ -0,0 +1,19 @@
+package question2;
+
+public class Driver {
+	static RightSkewedBinarySearchTree rsbt = new RightSkewedBinarySearchTree();
+	
+
+	public static void main(String[] args) {
+		RightSkewedBinarySearchTree.Node node = new RightSkewedBinarySearchTree.Node(50);
+        node.left = new RightSkewedBinarySearchTree.Node(30);
+        node.right = new RightSkewedBinarySearchTree.Node(60);
+        node.left.left = new RightSkewedBinarySearchTree.Node(10);
+        node.left.right = new RightSkewedBinarySearchTree.Node(40);
+
+        rsbt.rightSkewedTree(node);
+        rsbt.printTree(rsbt.getRootnode());
+
+	}
+
+}
